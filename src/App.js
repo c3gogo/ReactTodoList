0956f0e353e548161272d2040fb46571432975ed
Corentin <gogo.corentin@gmail.com>
2017-12-07 17:36:37 +0100

    Add a mock todo list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React, { Component } from "react";
+import uuid from 'uuid';
+
 import AddTodo from "./Components/AddTodo";
 import Todos from "./Components/Todos";
 import "./App.css";
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    };
+  }
+
+  getTodos() {
+    this.setState({
+      todos:  [
+        {
+          id: uuid.v4(),
+          title: "Test my todo list"
+        },
+
+        {
+          id: uuid.v4(),
+          title: "Test my todo list"
+        },
+        
+        {
+          id: uuid.v4(),
+          title: "Test my todo list"
+        },
+
+      ]
+    });
+  }
+
+
   render() {
     return (
       <div className="App">
